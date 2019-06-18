c35dd431de444ce2672e523a2e93bc8eca9d1663
Christopher Stolte <cstolte@scainc.com>
2019-06-18 08:44:42 -0400

    stashing hi histogram

--- com/sca/hem4/writer/excel/summary/HI_Histogram.py
+++ com/sca/hem4/writer/excel/summary/HI_Histogram.py
@@ -1,11 +1,9 @@
-import os
-import pandas as pd
-
-from com.sca.hem4.log import Logger
+from math import log10, floor
 from com.sca.hem4.writer.csv.BlockSummaryChronic import *
 from com.sca.hem4.writer.excel.ExcelWriter import ExcelWriter
 
-
+risklevel = 'risklevel'
+facilitycount = 'facilitycount'
 class HI_Histogram(ExcelWriter):
 
     def __init__(self, targetDir, facilityIds):
@@ -13,7 +11,7 @@ class HI_Histogram(ExcelWriter):
         self.categoryFolder = targetDir
         self.facilityIds = facilityIds
 
-        self.filename = os.path.join(targetDir, "histogram_risk.xlsx")
+        self.filename = os.path.join(targetDir, "hi_histogram_risk.xlsx")
 
     def getHeader(self):
         return ['Risk level', 'Population', 'Facility count']
@@ -21,19 +19,41 @@ class HI_Histogram(ExcelWriter):
     def generateOutputs(self):
         Logger.log("Creating " + self.name + " report...", None, False)
 
+        self.facilityMap = {}
+
+        # Data structure to keep track of the needed histogram values.
+        # There are 5 sub lists corresponding to the five buckets.
+        counts = [[0,0], [0,0], [0,0], [0,0], [0,0]]
+
         blocksummary_df = pd.DataFrame()
         for facilityId in self.facilityIds:
             targetDir = self.categoryFolder + "/" + facilityId
 
             blockSummaryChronic = BlockSummaryChronic(targetDir=targetDir, facilityId=facilityId)
             bsc_df = blockSummaryChronic.createDataframe()
+
+            bsc_df.sort_values(by=[mir], ascending=False, inplace=True)
+            foundMax = False
+            for index, row in bsc_df.iterrows():
+
+                if not foundMax and row[population] > 0:
+                    foundMax = True
+                    rounded = self.round_to_sigfig(row[mir])
+
+                    if rounded < 1e-6:
+                        counts[0][1] = counts[0][1] + 1
+                    if rounded >= 1e-6:
+                        counts[1][1] = counts[1][1] + 1
+                    if rounded >= 1e-5:
+                        counts[2][1] = counts[2][1] + 1
+                    if rounded >= 1e-4:
+                        counts[3][1] = counts[3][1] + 1
+                    if rounded >= 1e-3:
+                        counts[4][1] = counts[4][1] + 1
+
             blocksummary_df = blocksummary_df.append(bsc_df)
 
         blocksummary_df.drop_duplicates().reset_index(drop=True)
-
-        # Drop records that (are not user receptors AND have population = 0)
-        blocksummary_df.drop(blocksummary_df[(blocksummary_df.population == 0) & ("U" not in blocksummary_df.block)].index,
-                             inplace=True)
 
         aggs = {lat:'first', lon:'first', overlap:'first', elev:'first', utme:'first', rec_type:'first',
                 utmn:'first', hill:'first', fips:'first', block:'first', population:'first',
@@ -44,57 +64,43 @@ class HI_Histogram(ExcelWriter):
         # Aggregate concentration, grouped by FIPS/block
         risk_summed = blocksummary_df.groupby([fips, block]).agg(aggs)[blockSummaryChronic.getColumns()]
 
-        mir_row = risk_summed.loc[risk_summed[mir].idxmax()]
-        hi_resp_row = risk_summed.loc[risk_summed[hi_resp].idxmax()]
-        hi_live_row = risk_summed.loc[risk_summed[hi_live].idxmax()]
-        hi_neur_row = risk_summed.loc[risk_summed[hi_neur].idxmax()]
-        hi_deve_row = risk_summed.loc[risk_summed[hi_deve].idxmax()]
-        hi_repr_row = risk_summed.loc[risk_summed[hi_repr].idxmax()]
-        hi_kidn_row = risk_summed.loc[risk_summed[hi_kidn].idxmax()]
-        hi_ocul_row = risk_summed.loc[risk_summed[hi_ocul].idxmax()]
-        hi_endo_row = risk_summed.loc[risk_summed[hi_endo].idxmax()]
-        hi_hema_row = risk_summed.loc[risk_summed[hi_hema].idxmax()]
-        hi_immu_row = risk_summed.loc[risk_summed[hi_immu].idxmax()]
-        hi_skel_row = risk_summed.loc[risk_summed[hi_skel].idxmax()]
-        hi_sple_row = risk_summed.loc[risk_summed[hi_sple].idxmax()]
-        hi_thyr_row = risk_summed.loc[risk_summed[hi_thyr].idxmax()]
-        hi_whol_row = risk_summed.loc[risk_summed[hi_whol].idxmax()]
+        for index, row in risk_summed.iterrows():
+            rounded = self.round_to_sigfig(row[mir])
+            if rounded < 1e-6:
+                counts[0][0] = counts[0][0] + row[population]
+            if rounded >= 1e-6:
+                counts[1][0] = counts[1][0] + row[population]
+            if rounded >= 1e-5:
+                counts[2][0] = counts[2][0] + row[population]
+            if rounded >= 1e-4:
+                counts[3][0] = counts[3][0] + row[population]
+            if rounded >= 1e-3:
+                counts[4][0] = counts[4][0] + row[population]
+
+        # for risklev in counts:
+        #     print(str(risklev[0]) + " : " + str(risklev[1]))
 
         risks = [
-            ['mir', mir_row[4], mir_row[5], mir_row[9], mir_row[10]] if mir_row[10] > 0
-            else ['mir', '', '', 0, 0],
-            ['respiratory', hi_resp_row[4], hi_resp_row[5], hi_resp_row[9], hi_resp_row[11]] if hi_resp_row[11] > 0
-            else ['respiratory', '', '', 0, 0],
-            ['liver', hi_live_row[4], hi_live_row[5], hi_live_row[9], hi_live_row[12]] if hi_live_row[12] > 0
-            else ['liver', '', '', 0, 0],
-            ['neurological', hi_neur_row[4], hi_neur_row[5], hi_neur_row[9], hi_neur_row[13]] if hi_neur_row[13] > 0
-            else ['neurological', '', '', 0, 0],
-            ['developmental', hi_deve_row[4], hi_deve_row[5], hi_deve_row[9], hi_deve_row[14]] if hi_deve_row[14] > 0
-            else ['developmental', '', '', 0, 0],
-            ['reproductive', hi_repr_row[4], hi_repr_row[5], hi_repr_row[9], hi_repr_row[15]] if hi_repr_row[15] > 0
-            else ['reproductive', '', '', 0, 0],
-            ['kidney', hi_kidn_row[4], hi_kidn_row[5], hi_kidn_row[9], hi_kidn_row[16]] if hi_kidn_row[16] > 0
-            else ['kidney', '', '', 0, 0],
-            ['ocular', hi_ocul_row[4], hi_ocul_row[5], hi_ocul_row[9], hi_ocul_row[17]] if hi_ocul_row[17] > 0
-            else ['ocular', '', '', 0, 0],
-            ['endocrine', hi_endo_row[4], hi_endo_row[5], hi_endo_row[9], hi_endo_row[18]] if hi_endo_row[18] > 0
-            else ['endocrine', '', '', 0, 0],
-            ['hematological', hi_hema_row[4], hi_hema_row[5], hi_hema_row[9], hi_hema_row[19]] if hi_hema_row[19] > 0
-            else ['hematological', '', '', 0, 0],
-            ['immunological', hi_immu_row[4], hi_immu_row[5], hi_immu_row[9], hi_immu_row[20]] if hi_immu_row[20] > 0
-            else ['immunological', '', '', 0, 0],
-            ['skeletal', hi_skel_row[4], hi_skel_row[5], hi_skel_row[9], hi_skel_row[21]] if hi_skel_row[21] > 0
-            else ['skeletal', '', '', 0, 0],
-            ['spleen', hi_sple_row[4], hi_sple_row[5], hi_sple_row[9], hi_sple_row[22]] if hi_sple_row[22] > 0
-            else ['spleen', '', '', 0, 0],
-            ['thyroid', hi_thyr_row[4], hi_thyr_row[5], hi_thyr_row[9], hi_thyr_row[23]] if hi_thyr_row[23] > 0
-            else ['thyroid', '', '', 0, 0],
-            ['whole body', hi_whol_row[4], hi_whol_row[5], hi_whol_row[9], hi_whol_row[24]] if hi_whol_row[24] > 0
-            else ['whole body', '', '', 0, 0],
+            ['<1e-6', counts[0][0], counts[0][1]] if counts[0][1] > 0 else ['<1e-6', '', 0],
+            ['>=1e-6', counts[1][0], counts[1][1]] if counts[1][1] > 0 else ['>=1e-6', '', 0],
+            ['>=1e-5', counts[2][0], counts[2][1]] if counts[2][1] > 0 else ['>=1e-5', '', 0],
+            ['>=1e-4', counts[3][0], counts[3][1]] if counts[3][1] > 0 else ['>=1e-4', '', 0],
+            ['>=1e-3', counts[4][0], counts[4][1]] if counts[4][1] > 0 else ['>=1e-3', '', 0],
         ]
-        maxrisk_df = pd.DataFrame(risks, columns=[risktype, fips, block, population, risk])
+        histogram_df = pd.DataFrame(risks, columns=[risklevel, population, facilitycount]).astype(
+            dtype=int, errors='ignore')
 
         # Put final df into array
-        self.dataframe = maxrisk_df
+        self.dataframe = histogram_df
         self.data = self.dataframe.values
         yield self.dataframe
+
+    def round_to_sigfig(self, x, sig=1):
+        if x == 0:
+            return 0;
+
+        if math.isnan(x):
+            return float('NaN')
+
+        rounded = round(x, sig-int(floor(log10(abs(x))))-1)
+        return rounded
