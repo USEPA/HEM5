3d5d395290e8a68046e501e6da901aa7fcdf79b7
Christopher Stolte <cstolte@scainc.com>
2019-12-12 09:36:06 -0500

    added risk breakdown to comparer script

--- com/sca/hem4/compare/Hem3Comparer.py
+++ com/sca/hem4/compare/Hem3Comparer.py
@@ -7,7 +7,9 @@ from com.sca.hem4.writer.csv.hem3.Hem3AllInnerReceptors import Hem3AllInnerRecep
 from com.sca.hem4.writer.csv.hem3.Hem3AllOuterReceptors import Hem3AllOuterReceptors
 from com.sca.hem4.writer.csv.hem3.Hem3AllPolarReceptors import Hem3AllPolarReceptors
 from com.sca.hem4.writer.excel.MaximumIndividualRisks import MaximumIndividualRisks, value, parameter
+from com.sca.hem4.writer.excel.RiskBreakdown import RiskBreakdown, site_type
 from com.sca.hem4.writer.excel.hem3.Hem3MaximumIndividualRisks import Hem3MaximumIndividualRisks
+from com.sca.hem4.writer.excel.hem3.Hem3RiskBreakdown import Hem3RiskBreakdown
 
 hem3Dirname = r"\\sfudge-pc\HEM3_for_HEM4_compare\hem3_output_unittest\01043110000366247"
 hem4Dirname = r"C:\Git_HEM4\HEM4\output\Unit\01043110000366247"
@@ -76,7 +78,23 @@ class Hem3Comparer():
         diff_df = self.calculateNumericDiffs(hem3risks, hem4risks, joinColumns, diffColumns)
         risks_diff.appendToFile(diff_df)
 
-        
+        #---------- Risk breakdown -----------#
+        hem3File = "FAC1-NC_risk_breakdown.xlsx"
+        hem4File = "FAC1-NC_risk_breakdown.xlsx"
+        diffFile = "diff_risk_breakdown.xlsx"
+        joinColumns = [site_type, parameter, source_id, pollutant]
+        diffColumns = [value]
+        #------------------------------------------#
+        hem4risks = RiskBreakdown(targetDir=self.hem4Dir, facilityId=None, model=None, plot_df=None,
+                                           filenameOverride=hem4File)
+        hem3risks = Hem3RiskBreakdown(targetDir=self.hem3Dir, facilityId=None, model=None, plot_df=None,
+                                               filenameOverride=hem3File)
+        risks_diff = RiskBreakdown(targetDir=self.diff_target, facilityId=None, model=None, plot_df=None,
+                                            filenameOverride=diffFile)
+        risks_diff.writeHeader()
+        diff_df = self.calculateNumericDiffs(hem3risks, hem4risks, joinColumns, diffColumns)
+        risks_diff.appendToFile(diff_df)
+
         #---------- All outer receptors -----------#
         hem3File = "01043110000366247_all_outer_receptors.csv"
         hem4File = "01043110000366247_all_outer_receptors.csv"
@@ -94,34 +112,52 @@ class Hem3Comparer():
         diff_df = self.calculateNumericDiffs(hem3allouter, hem4allouter, joinColumns, diffColumns)
         allouter_diff.appendToFile(diff_df)
 
+
+
+
+
+
     # Note: for this method to work correctly, none of the columns in diffColumns can be
     # present in joinColumns
     def calculateNumericDiffs(self, hem3_entity, hem4_entity, joinColumns, diffColumns):
+
         # Percent Change = ((HEM4- HEM3)/HEM3) * 100
-        
-        differences = []
 
         hem4_df = hem4_entity.createDataframe()
         hem3_df = hem3_entity.createDataframe()
 
         merged_df = hem4_df.merge(hem3_df, on=joinColumns, suffixes=('', '_y'))
         for numericCol in diffColumns:
-            merged_df[numericCol] = 100*(merged_df[numericCol] -
-                      merged_df[numericCol+"_y"]) / merged_df[numericCol+"_y"]
+
+            merged_df[numericCol] = merged_df[numericCol].apply(self.scrub_zero)
+            merged_df[numericCol+"_y"] = merged_df[numericCol+"_y"].apply(self.scrub_zero)
+
+            hem3Value = merged_df[numericCol+"_y"]
+            hem4Value = merged_df[numericCol]
+
+            merged_df[numericCol] = 100*(hem4Value - hem3Value) / hem3Value
             merged_df[numericCol] = merged_df[numericCol].apply(self.round_to_sigfig, args=[3])
 
         merged_df.drop(list(merged_df.filter(regex='_y$')), axis=1, inplace=True)
         return merged_df
 
+    # Change 0 to "very small" so we can compute a percentage change...
+    def scrub_zero(self, x):
+        if x == 0:
+            return 0.000001
+        else:
+            return x
+
     def round_to_sigfig(self, x, sig=1):
         if x == 0:
-            return 0;
+            return 0
 
         if math.isnan(x):
             return float('NaN')
 
         rounded = round(x, sig-int(floor(log10(abs(x))))-1)
         return rounded
+
 
 comparer = Hem3Comparer(hem3Dirname, hem4Dirname)
 comparer.compare()
