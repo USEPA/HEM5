99bb5d6648457f195c894fec9bf380d805a94c03
Christopher Stolte <cstolte@scainc.com>
2020-02-12 14:48:03 -0500

    added logic to invoke Temporal output...currently user selections are spoofed.

--- com/sca/hem4/writer/csv/Temporal.py
+++ com/sca/hem4/writer/csv/Temporal.py
@@ -1,4 +1,8 @@
 import os
+import pandas as pd
+
+from com.sca.hem4.CensusBlocks import *
+from com.sca.hem4.writer.csv.AllInnerReceptors import *
 from com.sca.hem4.writer.csv.CsvWriter import CsvWriter
 
 class Temporal(CsvWriter):
@@ -12,15 +16,41 @@ class Temporal(CsvWriter):
 
         self.filename = os.path.join(targetDir, facilityId + "_temporal.csv")
 
+        self.resolution = 4 # should come from model.facops or similar...
+        self.seasonal = True # again, needs to come from model opts
+
     def getHeader(self):
-        return ['Fips', 'Block', 'Population', 'Lat', 'Lon', 'Pollutant', 'Emis_type', 'Overlap', 'Modeled',
-                'C_01', 'C_02', 'C_03', 'C_04 ', 'etc, depending on temporal variations selected']
+        defaultHeader = ['Fips', 'Block', 'Population', 'Lat', 'Lon', 'Pollutant', 'Emis_type', 'Overlap', 'Block type']
+        return self.extendsCols(defaultHeader)
+
+    def getColumns(self):
+        defaulsCols = [fips, block, population, lat, lon, pollutant, emis_type, overlap, block]
+        return self.extendsCols(defaulsCols)
+
+    def extendsCols(self, default):
+        n_seas = 4 if self.seasonal else 1
+        n_hrblk = round(24/self.resolution)
+        concCols = n_seas * n_hrblk
+
+        for c in range(1, concCols+1):
+            colnum = str(c)
+            if len(colnum) < 2:
+                colnum = "0" + colnum
+            colName = "C_" + colnum
+            default.append(colName)
+
+        return default
 
     def generateOutputs(self):
         """
         Do something with the model and plot data, setting self.headers and self.data in the process.
         """
+        dlist = []
+        col_list = self.getColumns()
+        innerconc_df = pd.DataFrame(dlist, columns=col_list)
 
-        # TODO
-        self.data = []
+        # dataframe to array
+        self.dataframe = innerconc_df
+        self.data = self.dataframe.values
+
         yield self.dataframe
