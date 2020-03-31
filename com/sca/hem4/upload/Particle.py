e42c8d5bb0dcbe2b6698c2f56499512ef2467ba4
Christopher Stolte <cstolte@scainc.com>
2020-03-31 12:36:27 -0400

    added validation for particle file

--- com/sca/hem4/upload/Particle.py
+++ com/sca/hem4/upload/Particle.py
@@ -4,7 +4,7 @@
 Created on Fri Aug 31 20:13:09 2018
 @author: d
 """
-
+from com.sca.hem4.log import Logger
 from com.sca.hem4.upload.DependentInputFile import DependentInputFile
 from tkinter import messagebox
 from com.sca.hem4.model.Model import *
@@ -18,13 +18,13 @@ class Particle(DependentInputFile):
 
     def __init__(self, path, dependency, facilities):
         DependentInputFile.__init__(self, path, dependency, facilities)
+        self.faclist_df = dependency
         self.dependency = dependency
         self.facilities = facilities
 
     def createDataframe(self):
 
-        faclist_df = self.dependency
-        facid_list = faclist_df[fac_id].tolist()
+        facid_list = self.faclist_df[fac_id].tolist()
         
         # Specify dtypes for all fields
         self.numericColumns = [part_diam,mass_frac,part_dens]
@@ -32,26 +32,60 @@ class Particle(DependentInputFile):
 
         # Read the particle file
         particle_allfacs = self.readFromPath((fac_id, source_id, part_diam,mass_frac, part_dens))
-        
-        
+
         # Subset the particle data to the facilities being modeled
         particle_df = particle_allfacs.loc[particle_allfacs[fac_id].isin(facid_list)]
+        self.dataframe = particle_df
+
+    def clean(self, df):
 
         # Default NaN to 0 and convert float64 values to decimal with 3 decimal places
-        particle_df.fillna({part_diam:0, mass_frac:0, part_dens:0})
-        particle_df[part_diam] = particle_df[part_diam].apply(lambda x: round(Decimal(x), 3))
-        particle_df[mass_frac] = particle_df[mass_frac].apply(lambda x: round(Decimal(x), 3))
-        particle_df[part_dens] = particle_df[part_dens].apply(lambda x: round(Decimal(x), 3))
+        cleaned = df.fillna({part_diam:0, mass_frac:0, part_dens:0})
+        cleaned[part_diam] = cleaned[part_diam].apply(lambda x: round(Decimal(x), 3))
+        cleaned[mass_frac] = cleaned[mass_frac].apply(lambda x: round(Decimal(x), 3))
+        cleaned[part_dens] = cleaned[part_dens].apply(lambda x: round(Decimal(x), 3))
+        cleaned[mass_frac] = cleaned[mass_frac] / 100
 
-        particle_df[mass_frac] = particle_df[mass_frac] / 100
+        cleaned.replace(to_replace={fac_id:{"nan":""}, source_id:{"nan":""}}, inplace=True)
+        cleaned = cleaned.reset_index(drop = True)
 
+        return cleaned
 
-        #check for mass frac sum to 1
-        fac_ids = particle_df[fac_id].tolist()
+    def validate(self, df):
+        # ----------------------------------------------------------------------------------
+        # Strict: Invalid values in these columns will cause the upload to fail immediately.
+        # ----------------------------------------------------------------------------------
+        if len(df.loc[(df[fac_id] == '')]) > 0:
+            Logger.logMessage("One or more facility IDs are missing in the HAP Emissions List.")
+            return None
+
+        if len(df.loc[(df[source_id] == '')]) > 0:
+            Logger.logMessage("One or more source IDs are missing in the HAP Emissions List.")
+            return None
+
+        for index, row in df.iterrows():
+
+            facility = row[fac_id]
+
+            if row[part_diam] <= 0:
+                Logger.logMessage("Facility " + facility + ": particle diameter value " + str(row[part_diam]) +
+                                  " out of range.")
+                return None
+            if row[mass_frac] < 0 or row[mass_frac] > 100:
+                Logger.logMessage("Facility " + facility + ": mass fraction value " + str(row[mass_frac]) +
+                                  " out of range.")
+                return None
+            if row[part_dens] < 0 or row[part_dens] > 1:
+                Logger.logMessage("Facility " + facility + ": particle density value " + str(row[part_dens]) +
+                                  " out of range.")
+                return None
+
+        # check for mass frac sum to 1
+        fac_ids = df[fac_id].tolist()
         incomplete = []
         for fac in set(fac_ids):
-            fac_search = particle_df[particle_df[fac_id] == fac]
-            sources = particle_df[particle_df[fac_id] == fac][source_id].tolist()
+            fac_search = df[df[fac_id] == fac]
+            sources = df[df[fac_id] == fac][source_id].tolist()
 
             for s in set(sources):
                 mass_fracs = fac_search[fac_search[source_id] == s][mass_frac].tolist()
@@ -59,29 +93,25 @@ class Particle(DependentInputFile):
                 if sum(mass_fracs) != 1:
                     incomplete.append(str(fac) + ': ' + str(s))
 
-
         if len(incomplete) > 0:
             messagebox.showinfo("Mass Fraction Error",
                                 "The mass fractions for " + ", ".join(incomplete)+
                                 " do not sum to 1. Please correct them in your "+
                                 "particle size file.")
-
-
+            return None
         else:
-            #check for unassigned particle
-            check_particle_assignment = set(particle_df[fac_id])
-
+            # check for unassigned particle
+            check_particle_assignment = set(df[fac_id])
 
             if check_particle_assignment != set(self.facilities):
                 particle_unassigned = (check_particle_assignment -
                                        set(self.facilities))
-
 
                 messagebox.showinfo("Unassigned particle size parameters", "" +
                                     " Line parameters for " +
                                     ", ".join(particle_unassigned) + " have not been" +
                                     " assigned. Please edit the 'FacilityID' column" +
                                     " in the Facilities List Options file.")
-
+                return None
             else:
-                self.dataframe = particle_df
+                return df
