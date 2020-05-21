cd810580f7826b85bae17e59cdea69afaa4fdc65
sfudge <sfudge.scaninc.com>
2020-05-20 21:29:26 -0400

    punchlist7

--- com/sca/hem4/FindMet.py
+++ com/sca/hem4/FindMet.py
@@ -33,10 +33,10 @@ def find_met(ylat, xlon, metlib_df):
     distance2fac = dist[index]
     surf_file = metlib_df['surffile'][index]
     upper_file = metlib_df['upperfile'][index]
-    surfyear = metlib_df['surfyear'][index]
+    surfyear = int(metlib_df['surfyear'][index])
     # Note: remove white space from surfcity and uacity, Aermod will not allow spaces in the city name
-    surfdata_str = str(metlib_df['surfwban'][index]) + " " + str(metlib_df['surfyear'][index]) + " " + str(metlib_df['surfcity'][index]).replace(" ","")
-    uairdata_str = str(metlib_df['uawban'][index]) + " " + str(metlib_df['surfyear'][index]) + " " + str(metlib_df['uacity'][index]).replace(" ","")
+    surfdata_str = str(metlib_df['surfwban'][index]) + " " + str(int(metlib_df['surfyear'][index])) + " " + str(metlib_df['surfcity'][index]).replace(" ","")
+    uairdata_str = str(metlib_df['uawban'][index]) + " " + str(int(metlib_df['surfyear'][index])) + " " + str(metlib_df['uacity'][index]).replace(" ","")
     prof_base = str(metlib_df['elev'][index])
     
     return surf_file, upper_file, surfdata_str, uairdata_str, prof_base, distance2fac, surfyear
