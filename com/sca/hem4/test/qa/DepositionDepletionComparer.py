43327341a51667d39ac166e9bd35713e0608c250
Christopher Stolte <cstolte@scainc.com>
2020-05-26 13:32:56 -0400

    added dep/depl comparer similar to concentration comparer

--- com/sca/hem4/test/qa/DepositionDepletionComparer.py
+++ com/sca/hem4/test/qa/DepositionDepletionComparer.py
@@ -12,13 +12,18 @@ rundir = r"C:/Users/Chris Stolte/IdeaProjects/HEM4/output/AAA"
 hapemis_path = rundir + "/Inputs/hapemis.xlsx"
 output_dir = rundir + "/" + facilityID
 acute = 'Y'
+
+# Runtype: 1 == both, 2 == dry only, 3 == wet only...see FacilityRunner#set_runtype()
 runtype = 1
+
+emistype = 'P'
+deptype = 'W'
 
 class ConcentrationComparer(ExcelWriter):
 
     def __init__(self, fac_id, source_id, pollutant, hapemisPath, output_dir):
 
-        filename = fac_id + "_concentrations_compared.xlsx"
+        filename = fac_id + "_dep_compared.xlsx"
         path = os.path.join(output_dir, filename)
 
         ExcelWriter.__init__(self, None, None)
@@ -38,10 +43,10 @@ class ConcentrationComparer(ExcelWriter):
     def getHeader(self):
 
         return ['Facility ID', 'Source ID', 'Pollutant', 'Receptor Type',
-                'Lat', 'Lon', 'UTME', 'UTMN', 'Retrieved Conc', 'Computed Conc', '% Change']
+                'Lat', 'Lon', 'UTME', 'UTMN', 'Retrieved Dep', 'Computed Dep', '% Change']
 
     def getColumns(self):
-        return [fac_id, source_id, pollutant, rec_type, lat, lon, utme, utmn, 'r_conc', 'c_conc', 'change']
+        return [fac_id, source_id, pollutant, rec_type, lat, lon, utme, utmn, 'r_dep', 'c_dep', 'change']
 
     def generateOutputs(self):
         rows = []
@@ -54,53 +59,63 @@ class ConcentrationComparer(ExcelWriter):
                                     (hapemis_df[pollutant] == self.pollutant)]
 
         # Aermod plotfile
+        plotfile_name = "plotfile_p" if emistype == 'P' else "plotfile_v"
+        plotfile_name += ".plt"
+
         facilityRunner = FacilityRunner(self.fac_id, None, False)
-        ppfile = open(self.output_dir + '/plotfile.plt', "r")
+        ppfile = open(self.output_dir + "/" + plotfile_name, "r")
         plot_df = facilityRunner.readplotf(ppfile, runtype)
         plot_df = plot_df.loc[plot_df[source_id] == self.source_id]
 
-        # multiply by the hapemis value and the conversion factor
-        plot_df[result] = plot_df[result].multiply(hapemis_df.iloc[0][emis_tpy])
-        plot_df[result] = plot_df[result].multiply(2000*0.4536/3600/8760)
+        plotcolumn = wdp if deptype == 'W' else ddp
+        column = wetdep if deptype == 'W' else drydep
 
-        # Compare plot values to polar concentrations produced by HEM4
+        # multiply by the hapemis value and the conversion factor
+        plot_df[plotcolumn] = plot_df[plotcolumn].multiply(hapemis_df.iloc[0][emis_tpy])
+        plot_df[plotcolumn] = plot_df[plotcolumn].multiply(2000*0.4536/3600/8760)
+        plot_df[plotcolumn] = plot_df[plotcolumn].multiply(hapemis_df.iloc[0][part_frac]) if emistype == 'P' else \
+                        plot_df[plotcolumn].multiply(1 - hapemis_df.iloc[0][part_frac])
+
+        # Compare plot values to polar dep values produced by HEM4
         plot_polar_df = plot_df.loc[plot_df[net_id] == 'POLGRID1']
         allpolar = AllPolarReceptors(targetDir=self.output_dir, facilityId=self.fac_id, model=None, plot_df=plot_df,
                                      acuteyn=acute)
         allpolar_df = allpolar.createDataframe()
-        allpolar_df = allpolar_df.loc[(allpolar_df[source_id] == self.source_id) & (allpolar_df[pollutant] == self.pollutant)]
+        allpolar_df = allpolar_df.loc[(allpolar_df[source_id] == self.source_id) & (allpolar_df[pollutant] == self.pollutant)
+                                      & (allpolar_df[emis_type] == emistype)]
         for index,row in allpolar_df.iterrows():
 
             utm_n, utm_e, zone, hemi, epsg = UTM.ll2utm(row[lat], row[lon])
             plot_row = plot_polar_df.loc[(plot_polar_df[utmn] == utm_n) & (plot_polar_df[utme] == utm_e)]
             plot_row = plot_row.iloc[0]
 
-            computed = self.round_to_sigfig(plot_row[result], 8)
-            retrieved = self.round_to_sigfig(row[conc], 8)
+            computed = self.round_to_sigfig(plot_row[plotcolumn], 8)
+            retrieved = self.round_to_sigfig(row[column], 8)
             diff = self.round_to_sigfig(100*((retrieved - computed) / retrieved), 3)
             record = {fac_id:self.fac_id, source_id:self.source_id, pollutant:self.pollutant, rec_type:"polar",
-                      lat:row[lat], lon:row[lon], utme:utm_e, utmn:utm_n, 'r_conc':retrieved, 'c_conc':computed,
+                      lat:row[lat], lon:row[lon], utme:utm_e, utmn:utm_n, 'r_dep':retrieved, 'c_dep':computed,
                       'change':diff}
             rows.append(record)
 
         # Compare plot values to inner concentrations produced by HEM4
         plot_inner_df = plot_df.loc[plot_df[net_id] != 'POLGRID1']
         allinner = AllInnerReceptors(targetDir=self.output_dir, facilityId=self.fac_id, model=None, plot_df=plot_df,
-                                         acuteyn=acute)
+                                     acuteyn=acute)
         allinner_df = allinner.createDataframe()
-        allinner_df = allinner_df.loc[(allinner_df[source_id] == self.source_id) & (allinner_df[pollutant] == self.pollutant)]
+        allinner_df = allinner_df.loc[(allinner_df[source_id] == self.source_id) & (allinner_df[pollutant] == self.pollutant)
+                                      & (allinner_df[emis_type] == emistype)]
         for index,row in allinner_df.iterrows():
 
             utm_n, utm_e, zone, hemi, epsg = UTM.ll2utm(row[lat], row[lon])
             plot_row = plot_inner_df.loc[(plot_inner_df[utmn] == utm_n) & (plot_inner_df[utme] == utm_e)]
             plot_row = plot_row.iloc[0]
 
-            computed = self.round_to_sigfig(plot_row[result], 8)
-            retrieved = self.round_to_sigfig(row[conc], 8)
+            computed = self.round_to_sigfig(plot_row[plotcolumn], 8)
+            retrieved = self.round_to_sigfig(row[column], 8)
             diff = self.round_to_sigfig(100*((retrieved - computed) / retrieved), 3)
             record = {fac_id:self.fac_id, source_id:self.source_id, pollutant:self.pollutant, rec_type:"block",
-                  lat:row[lat], lon:row[lon], utme:utm_e, utmn:utm_n, 'r_conc':retrieved, 'c_conc':computed,
-                  'change':diff}
+                      lat:row[lat], lon:row[lon], utme:utm_e, utmn:utm_n, 'r_dep':retrieved, 'c_dep':computed,
+                      'change':diff}
             rows.append(record)
 
         # Return results
