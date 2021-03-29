0817ff14a3da8dee0f0187758abd3187f673cd89
sfudge <sfudge.scaninc.com>
2021-03-28 20:23:52 -0400

    Start of acs reformat tool

---
+++ com/sca/hem4/ej/tools/Convert2018ACSto2019Format.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 28 19:52:45 2021
+
+@author: Steve Fudge
+"""
+
+import pandas as pd
+
+acs2018_file = r"C:\Git_HEM4\ACS data backup\acs_2018.xlsx"
+reformatted_acs2018_file = r"C:\Git_HEM4\ACS data backup\acs_2018_reformat.xlsx"
+
+
+columns2018 = ['STCNTRBG', 'TOTALPOP', 'PCT_MINORITY', 'PCT_WHITE', 'PCT_BLACK', 'PCT_HISP', 'PCT_ASIAN',
+               'PCT_AMERIND', 'PCT_HAWPAC', 'PCT_OTHER_RACE', 'PCT_TWOMORE', 'PCT_AGE_LT18', 'PCT_AGE_GT64',
+               'POV_UNIVERSE_FRT', 'PCT_LOWINC', 'PCT_INC_POV_LT50', 'PCT_INC_POV_50TO99', 'EDU_UNIVERSE',
+               'PCT_EDU_LTHS', 'PCT_LINGISO', 'PCT_NON_HISP', 'PCT_NON_HISP_WHITE', 'PCT_NON_HISP_BLACK',
+               'PCT_NON_HISP_AMERIND', 'PCT_NON_HISP_OTHER', 'PCT_HISP_WHITE', 'PCT_HISP_BLACK', 'PCT_HISP_AMERIND',
+               'PCT_HISP_OTHER', 'POVERTY_FLAG', 'EDUCATION_FLAG', 'LING_ISO_FLAG', 'FRACT_25UP', 'HOUSEHOLDS']
+
+columns2019 = ['STCNTRBG', 'TOTALPOP', 'PCT_MINORITY', 'PCT_WHITE', 'PCT_BLACK', 'PCT_AMIND', 'PCT_OTHER_RACE', 'PCT_HISP',
+               'PCT_AGE_LT18', 'PCT_AGE_GT64', 'PCT_LOWINC', 'PCT_POV', 'AGE_25UP', 'PCT_EDU_LTHS', 'PCT_LINGISO', 'AGE_UNIVERSE',
+               'POV_UNIVERSE', 'EDU_UNIVERSE', 'ISO_UNIVERSE', 'POVERTY_FLAG', 'EDUCATION_FLAG', 'LING_ISO_FLAG']
+
