d76790d91902ee857f7d6f728e3c46a3bd3065d0
sfudge <sfudge.scaninc.com>
2021-06-21 13:42:41 -0400

    EJ dash bar graph formatting

--- com/sca/hem4/dash/EJdash.py
+++ com/sca/hem4/dash/EJdash.py
@@ -606,12 +606,10 @@ class EJdash():
                 hoverdata = {group: ':.1f',
                              group +' Pop': ':0f'}
                                 
-                # Set bar width to 30 pixels per facility
-                autowidth = numFacs*30
-                fig = px.bar(dff, x='Facility', y=yaxis, height=800, width=autowidth, text = text,opacity=.7, hover_data=hoverdata)
-                fig.update_yaxes(type = type, title_text=ytitle, title_font=dict(size = 16, color = 'black'))
-                fig.update_xaxes(title_text='<b>Facility</b>', title_font=dict(size = 16, color = 'black'), tickangle=40,
-                                 type = 'category', range = (-.5, numFacs))
+                fig = px.bar(dff, x='Facility', y=yaxis, height=800, width=1500, text = text,opacity=.7, hover_data=hoverdata)
+                fig.update_yaxes(type = type, title_text=ytitle, title_font=dict(size = 16, color = 'black', automargin=True))
+                fig.update_xaxes(title_text='<b>Facility</b>', title_font=dict(size = 16, color = 'black', automargin=True), tickangle=40,
+                                 type = 'category', range = (-.5, min(numFacs,50)))
                 fig.update_layout(title = title, title_font=dict(size = 22, color = 'black'),
                                   clickmode="event+select", hovermode = 'closest',
                                   margin=dict(r=100))
