eb6d53f4651a9510eea5b4e0dbe5f8cdacbbe682
sfudge <sfudge.scaninc.com>
2022-08-21 21:29:35 -0400

    Finished incorporating new county default method. Included 2020 ACS

--- com/sca/hem4/ej/data/ACSDataset.py
+++ com/sca/hem4/ej/data/ACSDataset.py
@@ -55,13 +55,13 @@ class ACSDataset:
 
     def getColumns(self):
         return ['STCNTRBG', 'TOTALPOP', 'PCT_MINORITY', 'PCT_WHITE', 'PCT_BLACK', 'PCT_AMIND', 'PCT_OTHER_RACE', 'PCT_HISP',
-                'PCT_AGE_LT18', 'PCT_AGE_GT64', 'PCT_LOWINC', 'PCT_POV', 'AGE_25UP', 'PCT_EDU_LTHS', 'PCT_LINGISO', 'AGE_UNIVERSE',
+                'PCT_AGE_LT18', 'PCT_AGE_GT64', 'PCT_LOWINC', 'PCT_POV', 'PCT_EDU_LTHS', 'PCT_LINGISO',
                 'POV_UNIVERSE', 'EDU_UNIVERSE', 'ISO_UNIVERSE', 'POVERTY_FLAG', 'EDUCATION_FLAG', 'LING_ISO_FLAG']
 
     def createDataframe(self):
         # Type setting for reading
         self.numericColumns = ['TOTALPOP', 'PCT_MINORITY', 'PCT_WHITE', 'PCT_BLACK', 'PCT_AMIND', 'PCT_OTHER_RACE', 'PCT_HISP',
-                               'PCT_AGE_LT18', 'PCT_AGE_GT64', 'PCT_LOWINC', 'PCT_POV', 'AGE_25UP', 'PCT_EDU_LTHS', 'PCT_LINGISO', 'AGE_UNIVERSE',
+                               'PCT_AGE_LT18', 'PCT_AGE_GT64', 'PCT_LOWINC', 'PCT_POV', 'PCT_EDU_LTHS', 'PCT_LINGISO',
                                'POV_UNIVERSE', 'EDU_UNIVERSE', 'ISO_UNIVERSE']
         self.strColumns = ['STCNTRBG', 'POVERTY_FLAG', 'EDUCATION_FLAG', 'LING_ISO_FLAG']
 
