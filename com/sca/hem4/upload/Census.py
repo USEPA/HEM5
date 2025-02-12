c535b1b76fae8be9fac2e9b132605831000b47ba
Steve Fudge <sfudge@scainc.com>
2025-02-12 17:06:04 -0500

    added try/except around upload of census

--- com/sca/hem4/upload/Census.py
+++ com/sca/hem4/upload/Census.py
@@ -22,8 +22,15 @@ class Census(InputFile):
                           'hill':pl.Float64}
 
         # Create polars lazyframe of the national census data
-        self.dataframe = self.readFromPathCsvPolars(self.colnames)
-        
+        try:
+            self.dataframe = self.readFromPathCsvPolars(self.colnames)
+        except BaseException as ex:
+            message = ('\nFailed to read the census file because the file is not properly formatted.\n' 
+                       + 'This model run will stop. Please correct the census file before rerunning.\n'
+                       + 'The detailed error message is:\n\n'
+                       + str(ex) + '\n')
+            Logger.logMessage(message)
+            return None
 
 
     def validate(self, df):
@@ -40,7 +47,6 @@ class Census(InputFile):
         # Get the column names with missing values for each row
         missing_cols_per_row = (p_df[rows_with_missing].isnull()
                                 .apply(lambda x: x.index[x].tolist(), axis=1))
-
                 
         if len(missing_cols_per_row) > 0:
                         
@@ -49,9 +55,10 @@ class Census(InputFile):
                              + 'The missing values are identified below: \n')
             Logger.logMessage(message)
             for index, value in missing_cols_per_row.items():
-                index_comma = f"{index:,}"
+                # iinenum must account for header and 0-index
+                linenum = f"{index+2:,}"
                 value_str = ", ".join(value)
-                message = 'Line ' + str(index_comma) + ' has missing values in columns [' + value_str + ']'
+                message = 'Line ' + str(linenum) + ' has missing values.'
                 Logger.logMessage(message)
             
             return None
