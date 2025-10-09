b74242b40f379918d67243f743b1bbde5d6257ce
Steve Fudge <sfudge@scainc.com>
2025-10-09 15:43:27 -0400

    pyinstaller changes for hem5.1

--- installer/pyinstaller_hook_dir/PyinstallerHookFile.py
+++ installer/pyinstaller_hook_dir/PyinstallerHookFile.py
@@ -9,4 +9,5 @@ import os
 
 # Set the PROJ_LIB environment variable that is required by pyproj.
 # This setting only holds during the course of the process.
+
 os.environ['PROJ_LIB'] = os.path.join(os.getcwd(), 'share')
